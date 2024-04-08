b97ee7d4c329715e76828a0c807aa39ddbac164c
Jake Davis <jacobdavis620@gmail.com>
2024-04-08 14:30:59 -0500

    rename reducerPath

--- src/store/server/quoteApi.ts
+++ src/store/server/quoteApi.ts
@@ -2,8 +2,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { IAnimeQuote } from 'types/animeQuote.types';
 
 export const quoteApi = createApi({
-  reducerPath: 'pokemonApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
+  reducerPath: 'quoteApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://192.168.4.29:9001' }),
   endpoints: builder => ({
     getAnimeQuote: builder.query<IAnimeQuote, void>({
       query: () => `quote`,
